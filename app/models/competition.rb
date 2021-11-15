133fb61a097691b09e8e51b88201297a9e14b9e7
Christian Valesares <cvalesares@gmail.com>
2021-11-15 12:19:20 -0500

    add competition show spec

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ApplicationRecord
   has_many :competition_teams
   has_many :teams, through: :competition_teams
+
+  def self.average_player_age
+    Team.joins(:players).average(:age)
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Competition, type: :model do
     it { should have_many :competition_teams }
     it { should have_many :teams }
   end
+
+  describe "methods" do
+    describe "::average_player_age" do
+      it "can find the average age of all players" do
+        competition1 = Competition.create!(name: "Stanely Cup", location: "Canada", sport: "Hockey")
+        team1 = competition1.teams.create!(hometown: "Tampa", nickname: "Bolts")
+        team2 = competition1.teams.create!(hometown: "Raleigh", nickname: "Canes")
+        player1 = team1.players.create!(name: "Brayden Point", age: 26)
+        player2 = team1.players.create!(name: "Stephen Stamkos", age: 36)
+        player3 = team2.players.create!(name: "Sebastian Aho", age: 28)
+
+        expect(Competition.average_player_age).to eq(30)
+      end
+    end
+  end
 end
